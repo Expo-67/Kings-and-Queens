bf41335ce795f4961baabe03069c4500ec819687
Expo-67 <145459052+Expo-67@users.noreply.github.com>
2025-01-14 21:43:15 +0300

    setup config

---
+++ Frontend/src/app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="text-5xl bg-slate-950">
+      Navbar goes here guys its Sakka again!
+    </div>
+  );
+};
+
+export default Navbar;
